3000b96918a702c721d57023a09b9377721722a9
unclemirror <unclemirror526@gmail.com>
2025-10-22 09:59:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,5 +19,19 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
+##-----------------Replace OpenClash------------------
+git clone --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
+rm -rf feeds/luci/applications/luci-app-openclash
+mv /tmp/OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
+rm -rf /tmp/OpenClash
+
+##-----------------Add OpenClash meta core------------------
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+
 ##-----------------Manually set CPU frequency for MT7986A-----------------
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
