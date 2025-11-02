aacf06f1ca6f8a0c6204429521ca96be44615aa5
unclemirror <unclemirror526@gmail.com>
2025-11-02 21:31:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -44,11 +44,10 @@ rm -rf /tmp/Cloudflared
 
 ##-----------------Replace latest cloudflared------------------
 rm -rf feeds/packages/net/cloudflared
-curl -sL -m 30 --retry 2 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -o /tmp/clash.tar.gz
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+curl -sL -m 30 --retry 2 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -o /tmp/Cloudflared
+mv /tmp/Cloudflared/cloudflared-linux-arm64 /usr/sbin/cloudflared
+chmod +x /usr/sbin/cloudflared
+rm -rf /tmp/Cloudflared
 
 ##-----------------Manually set CPU frequency for MT7986A-----------------
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
