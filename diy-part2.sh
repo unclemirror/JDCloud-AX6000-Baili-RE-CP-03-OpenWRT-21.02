731b0bd9b2344010003a119344a0a6c4649facd8
unclemirror <unclemirror526@gmail.com>
2025-11-02 23:58:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,25 +23,21 @@
 rm -rf feeds/packages/net/open-app-filter
 rm -rf feeds/luci/applications/luci-app-appfilter
 
-##-----------------Replace latest OpenClash------------------
-git clone --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
-rm -rf feeds/luci/applications/luci-app-openclash
-mv /tmp/OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
-rm -rf /tmp/OpenClash
-
 ##-----------------Add latest OpenClash meta core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
+tar zxvf /tmp/clash.tar.gz -C /tmp
+chmod +x /tmp/clash
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta
+rm -rf /tmp/clash.tar.gz
 
 ##-----------------Replace latest cloudflared------------------
 rm -rf feeds/packages/net/cloudflared
-curl -sL -m 30 --retry 2 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -o /tmp/cloudflared-linux-arm64
-mv /tmp/cloudflared-linux-arm64 /usr/sbin/cloudflared
-chmod +x /usr/sbin/cloudflared
+curl -sL -m 30 --retry 2 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -o /tmp/cloudflared
+chmod +x /tmp/cloudflared
+mkdir -p feeds/luci/applications/luci-app-cloudflared
+mv /tmp/cloudflared feeds/luci/applications/luci-app-cloudflared/cloudflared
+chmod +x feeds/luci/applications/luci-app-cloudflared/cloudflared
 
 ##-----------------Manually set CPU frequency for MT7986A-----------------
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
