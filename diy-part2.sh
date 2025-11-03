1dcbc616747a7baf1ae234489f4bee0ebe20d56a
unclemirror <unclemirror526@gmail.com>
2025-11-03 11:22:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,16 +27,16 @@ rm -rf feeds/luci/applications/luci-app-appfilter >/dev/null 2>&1
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
+mkdir -p package/OpenClash/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash package/OpenClash/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 
 ##-----------------Replace latest cloudflared------------------
 rm -rf feeds/packages/net/cloudflared >/dev/null 2>&1
 curl -sL -m 30 --retry 2 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -o /tmp/cloudflared
 chmod +x /tmp/cloudflared >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-cloudflared/root/etc/client
-mv /tmp/cloudflared feeds/luci/applications/luci-app-cloudflared/root/etc/client/cloudflared >/dev/null 2>&1
+mkdir -p package/CloudFlared/luci-app-cloudflared/root/etc/client
+mv /tmp/cloudflared package/CloudFlared/luci-app-cloudflared/root/etc/client/cloudflared >/dev/null 2>&1
 
 ##-----------------Manually set CPU frequency for MT7986A-----------------
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
